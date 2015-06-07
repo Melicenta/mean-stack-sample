b43bfe9e279c096d98000a57398f0d56f6475393
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-08 02:37:00 +0300

    index template added

--- src/main/modules/application.js
+++ src/main/modules/application.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 express()
 
+    .set('view engine', 'jade')
+
+    .set('views', 'src/main/resources/templates')
+
     .get('/(*)', function (request, response) {
-      response.send('Web application page');
+      response.render("index", {
+        application: {
+          title: 'MEAN Stack Sample'
+        },
+        system: {
+          path: '/'
+        }
+      });
     })
 
     .listen(process.env.PORT);
