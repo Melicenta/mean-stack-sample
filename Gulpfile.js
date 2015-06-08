f2711fbf93cd70f42504d501831dafaf8f0cdb1b
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-08 06:11:03 +0300

    gulpfile less task added

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    less = require('gulp-less'),
+    LessPluginAutoprefix = require('less-plugin-autoprefix'),
+    LessPluginClean = require('less-plugin-clean-css');
 
-gulp.task('dev', ['browserify'], function () {
+gulp.task('dev', ['browserify', 'less'], function () {
 
 });
 
@@ -14,6 +17,20 @@ gulp.task('browserify', function() {
       }))
       .pipe(concat('bundle.js'))
       .pipe(gulp.dest('src/main/resources/static/scripts'));
+});
+
+gulp.task('less', function () {
+  autoprefix = new LessPluginAutoprefix({
+    browsers: ["last 2 versions"]
+  });
+  clean = new LessPluginClean({
+    advanced: true
+  });
+  gulp.src(['src/main/resources/static/styles/**/*.js'])
+      .pipe(less({
+        plugins: [autoprefix, clean]
+      }))
+      .pipe(gulp.dest('src/main/resources/static/styles'));
 });
 
 gulp.task('default', ['dev']);
