0f19742dbc21b6bb5240e956d41c491e7f99f5a1
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-08 04:22:24 +0300

    gulp build file added

---
+++ build.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
+
+gulp.task('dev', ['browserify'], function () {
+
+});
+
+gulp.task('browserify', function() {
+  gulp.src(['scr/main/resources/static/scripts/main.js'])
+      .pipe(browserify({
+        insertGlobals: true,
+        debug: false
+      }))
+      .pipe(concat('bundle.js'))
+      .pipe(gulp.dest('scr/main/resources/static/scripts'));
+});
+
+gulp.task('default', ['dev']);
