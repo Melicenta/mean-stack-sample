e162a604947c1f59ebd4a66a75e8d79dd2b2be1d
IlyaGubarev <ilya.gubarev@gmail.com>
2015-06-08 18:17:50 +0300

    gulpfile.js updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,24 +2,50 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     less = require('gulp-less'),
+    rimraf = require('rimraf'),
     LessPluginAutoprefix = require('less-plugin-autoprefix'),
     LessPluginClean = require('less-plugin-clean-css');
 
-gulp.task('dev', ['browserify', 'less'], function () {
+gulp.task('dev', ['clean', 'modules', 'templates', 'images', 'scripts', 'styles'], function () {
 
 });
 
-gulp.task('browserify', function() {
+gulp.task('clean', function () {
+
+  rimraf('./build');
+});
+
+gulp.task('modules', function () {
+
+  gulp.src(['src/main/modules'])
+    .pipe(gulp.dest(['build/modules']));
+});
+
+gulp.task('templates', function () {
+
+  gulp.src(['src/main/resources/templates'])
+    .pipe(gulp.dest(['build/resources/templates']));
+});
+
+gulp.task('images', function () {
+
+  gulp.src(['src/main/resources/static/images'])
+    .pipe(gulp.dest(['build/resources/static/images']));
+});
+
+gulp.task('scripts', function() {
+
   gulp.src(['src/main/resources/static/scripts/application.js'])
       .pipe(browserify({
         insertGlobals: true,
         debug: false
       }))
       .pipe(concat('bundle.js'))
-      .pipe(gulp.dest('src/main/resources/static/scripts'));
+      .pipe(gulp.dest('build/resources/static/scripts'));
 });
 
-gulp.task('less', function () {
+gulp.task('styles', function () {
+
   autoprefix = new LessPluginAutoprefix({
     browsers: ["last 2 versions"]
   });
@@ -30,7 +56,7 @@ gulp.task('less', function () {
       .pipe(less({
         plugins: [autoprefix, clean]
       }))
-      .pipe(gulp.dest('src/main/resources/static/styles'));
+      .pipe(gulp.dest('build/resources/static/styles'));
 });
 
 gulp.task('default', ['dev']);
